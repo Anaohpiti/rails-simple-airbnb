95b841626c362a3862b3c33764cb28286d7cb609
Jean Laine <lainecaroff.jean@gmail.com>
2020-11-11 16:40:38 +0100

    trying to implement the search bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "creating 4 fake flats"
+
+Flat.destroy_all
+
+4.times do Flat.create!(
+  name: Faker::Movies::HarryPotter.character,
+  address: Faker::Movies::HarryPotter.location,
+  description: Faker::Movies::HarryPotter.quote,
+  price_per_night: rand(50..100),
+  number_of_guests: rand(4),
+)
+end
+
+p "4 fake flats create"
